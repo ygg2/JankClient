13df3d0655fca66a7072f870540b8bf5ac7e920e
MathMan05 <mathmanrm@gmail.com>
2025-09-27 14:14:00 -0500

    webhook fix

--- src/webpage/webhooks.ts
+++ src/webpage/webhooks.ts
@@ -66,6 +66,9 @@ async function webhookMenu(
 	}
 
 	const makeHook = (hook: webhookType) => {
+		//TODO remove once the server fixes this bug
+		hook.url ||= `${guild.info.api}/webhooks/${guild.id}/${hook.token}`;
+
 		const div = document.createElement("div");
 		div.classList.add("flexltr", "webhookArea");
 		const pfp = document.createElement("img");
