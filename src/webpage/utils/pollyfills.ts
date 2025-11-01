f89329af4546a5854ab60153f0ed0cc7e1d46742
MathMan05 <mathmanrm@gmail.com>
2025-10-31 20:40:59 -0500

    new polyfill

--- src/webpage/utils/pollyfills.ts
+++ src/webpage/utils/pollyfills.ts
@@ -1,5 +1,5 @@
 //So... there are polyfills for letting some older browsers to run Fermi, but it's not going to be anywhere near perfect with the CSS
-Set.prototype.intersection ||= function <T>(set: Set<T>) {
+Set.prototype.intersection ??= function <T>(set: Set<T>) {
 	const newSet = new Set();
 	for (const elm of this) {
 		if (set.has(elm)) {
@@ -8,7 +8,7 @@ Set.prototype.intersection ||= function <T>(set: Set<T>) {
 	}
 	return newSet;
 };
-Set.prototype.difference ||= function <T>(set: Set<T>) {
+Set.prototype.difference ??= function <T>(set: Set<T>) {
 	const newSet = new Set();
 	for (const elm of this) {
 		if (!set.has(elm)) {
@@ -17,7 +17,7 @@ Set.prototype.difference ||= function <T>(set: Set<T>) {
 	}
 	return newSet;
 };
-Set.prototype.symmetricDifference ||= function <T>(set: Set<T>) {
+Set.prototype.symmetricDifference ??= function <T>(set: Set<T>) {
 	const newSet = new Set();
 	for (const elm of this) {
 		if (!set.has(elm)) {
@@ -31,9 +31,26 @@ Set.prototype.symmetricDifference ||= function <T>(set: Set<T>) {
 	}
 	return newSet;
 };
-Set.prototype.isDisjointFrom ||= function <T>(set: Set<T>) {
+Set.prototype.isDisjointFrom ??= function <T>(set: Set<T>) {
 	return this.intersection(set).size === 0;
 };
-Set.prototype.union ||= function <T>(set: Set<T>) {
+Set.prototype.union ??= function <T>(set: Set<T>) {
 	return new Set([...this, ...set]);
 };
+
+function defineItter(itter: typeof Iterator) {
+	itter.prototype.map ??= function* map<U>(
+		callbackfn: (value: unknown, index: number) => U,
+	): IteratorObject<U, undefined, unknown> {
+		let i = 0;
+		for (const thing of this) {
+			yield callbackfn(thing, i);
+			i++;
+		}
+	};
+}
+if ("Iterator" in globalThis) {
+	defineItter(globalThis.Iterator);
+} else {
+	defineItter("".matchAll(/6/g).constructor as typeof Iterator);
+}
