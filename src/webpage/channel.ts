c26b35d2f90e54fabfe288ab0c6eb420a4a925e7
MathMan05 <mathmanrm@gmail.com>
2026-02-07 14:33:29 -0600

    fix perm check

--- src/webpage/channel.ts
+++ src/webpage/channel.ts
@@ -205,7 +205,7 @@ class Channel extends SnowFlake {
 			},
 			{
 				visible: function () {
-					return this.hasPermission("MANAGE_CHANNELS");
+					return this.hasPermission("MANAGE_CHANNELS") || this.owner_id === this.localuser.user.id;
 				},
 				icon: {
 					css: "svg-settings",
